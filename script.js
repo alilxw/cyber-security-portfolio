a09db144fa7184ae82b133668db0f9889cb3dcef
alilxw <84860730+alilxw@users.noreply.github.com>
2026-03-28 03:30:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -174,26 +174,4 @@ setInterval(() => {
 }, 3000);
 
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#%&@$0123456789";
-
-document.querySelectorAll(".decrypt-label").forEach(label => {
-    label.onmouseover = event => {
-        let iteration = 0;
-        const interval = setInterval(() => {
-            event.target.innerText = event.target.innerText
-                .split("")
-                .map((letter, index) => {
-                    if(index < iteration) {
-                        return event.target.dataset.value[index];
-                    }
-                    return letters[Math.floor(Math.random() * 40)];
-                })
-                .join("");
-
-            if(iteration >= event.target.dataset.value.length) {
-                clearInterval(interval);
-            }
-            iteration += 1 / 3;
-        }, 30);
-    };
 });
